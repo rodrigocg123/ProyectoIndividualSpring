46a289b2912b1b84f431e44ff4da3c306aa898da
Rodrigo <rocasgut@gmail.com>
2025-03-13 10:10:41 +0100

    iniciar proyecto

---
+++ Spring /ascender/src/test/java/es/santander/ascender/IndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
